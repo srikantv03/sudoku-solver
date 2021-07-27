004d1aea4591515438373541cf6f17b5a45021df
Srikant V <sv092003@gmail.com>
2021-07-26 20:29:01 -0400

    can solve basix 4x4

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import numpy as np
+
+def hasBlanks(puzzle):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle)):
+            if puzzle[i][j] == 0:
+                return True
+    return False
+
 def getPuzzle():
     puzzle = list()
     n = int(input())
@@ -6,12 +15,67 @@ def getPuzzle():
         puzzle.append(row)
     return puzzle
 
-# def getSquare(row, col, puzzle):
-#
-# def getRow(row, col, puzzle):
-#
-# def getColumn(row, col, puzzle):
-#     for i in range(len(puzzle)):
+def getSquare(row, col, puzzle):
+    square = list()
+    squareSide = int(np.sqrt(len(puzzle)))
+    rowSect = int(row/squareSide)
+    colSect = int(col/squareSide)
+    for i in range(rowSect * squareSide, rowSect * squareSide + squareSide):
+        for j in range(colSect * squareSide, colSect * squareSide + squareSide):
+            square.append(puzzle[i][j])
+    return square
+
+def getRow(row, col, puzzle):
+    returnArr = list()
+    for i in range(len(puzzle)):
+        returnArr.append(puzzle[row][i])
+    return returnArr
+
+def getColumn(row, col, puzzle):
+    returnArr = list()
+    for i in range(len(puzzle)):
+        returnArr.append(puzzle[i][col])
+    return returnArr
 
 
-print(getPuzzle())
+def makeNotes(puzzle):
+    nums = range(1, len(puzzle) + 1)
+    notes = list()
+    for i in range(len(puzzle)):
+        notes.append([])
+        for j in range(len(puzzle)):
+            if (puzzle[i][j] != 0):
+                notes[i].append([0])
+            else:
+                notAllowed = list(set(getSquare(i, j, puzzle) + getRow(i, j, puzzle) + getColumn(i, j, puzzle)))
+                notes[i].append(list(np.setdiff1d(nums, notAllowed)))
+    return notes
+
+def solve(puzzle):
+    amtFixed = 0
+    notes = makeNotes(puzzle)
+    for i in range(len(notes)):
+        for j in range(len(notes)):
+            if(len(notes[i][j]) == 1 and notes[i][j][0] != 0):
+                amtFixed += 1
+                puzzle[i][j] = notes[i][j][0]
+    if amtFixed == 0:
+        return puzzle
+    if hasBlanks(puzzle):
+        return(solve(puzzle))
+    else:
+        return puzzle
+
+def display(puzzle):
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle)):
+            print(str(puzzle[i][j]), end= " ")
+        print('\n')
+
+puzzle = getPuzzle()
+
+display(solve(puzzle))
+
+
+
+
