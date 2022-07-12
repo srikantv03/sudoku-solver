78bef3880ec5ed52dafe7b343f7c80228b50ea0d
Srikant Vasudevan <sv092003@gmail.com>
2022-07-12 17:36:09 -0400

    python setup information

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Sudoku-Solver',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Srikant Vasudevan',
+    author_email='',
+    description=''
+)
